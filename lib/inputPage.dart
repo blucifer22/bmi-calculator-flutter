4deaf08a54716372e50cc9fb4df83a51454cac38
Marc Chmielewski <54959001+blucifer22@users.noreply.github.com>
2020-05-05 12:26:30 -0400

    Restructuring filetree

--- lib/inputPage.dart
+++ lib/inputPage.dart
@@ -2,6 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import 'cardContent.dart';
+import 'resuableCard.dart';
+
 const double bottomContainerHeight = 80.0;
 const Color cardColor = Color(0xFF1D1E33);
 const Color bottomContainerColor = Color(0xFFEB1555);
@@ -75,51 +78,6 @@ class _InputPageState extends State<InputPage> {
           height: bottomContainerHeight,
         ),
       ]),
-    );
-  }
-}
-
-class CardContent extends StatelessWidget {
-  CardContent({@required this.text, @required this.icon});
-  final String text;
-  final IconData icon;
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Icon(
-          icon,
-          size: 80.0,
-        ),
-        SizedBox(
-          height: 15.0,
-        ),
-        Text(
-          text,
-          style: TextStyle(
-            fontSize: 18.0,
-            color: Color(0xFF8D8E98),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class ReusableCard extends StatelessWidget {
-  final Color color;
-  final Widget cardChild;
-  ReusableCard({@required this.color, this.cardChild});
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: cardChild,
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color: color,
-      ),
     );
   }
 }
